ad7ce163e5f5374645fb2c783baa30898b520a63
Marcus Krohné <marcuskrohne@gmail.com>
2023-02-05 17:04:15 +0100

    Update

--- MSTest/ConsoleApp/Services/FileManager.cs
+++ MSTest/ConsoleApp/Services/FileManager.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp.Services
+﻿namespace ConsoleApp.Services
 {
-    public class FileManager
+    public class FileService
     {
         public string FilePath { get; set; } = string.Empty;
 
